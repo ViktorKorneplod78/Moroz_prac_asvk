acdaf4fbe98921d0fa7e38f65f7a336b8e06a14e
Viktor Korneplod <m.v.korneplod@gmail.com>
2024-11-05 11:55:48 +0300

    hawk tuah

--- 20241105/1/prog.py
+++ 20241105/1/prog.py
@@ -1,24 +1,19 @@
 class Omnibus:
-    attrs = dict()
-
-    def __setattr__(self, name, value):
-        if name not in self.__dict__:
-            super().__setattr__(name, value)
-            if name in Omnibus.attrs:
-                Omnibus.attrs[name] += 1
-            else:
-                Omnibus.attrs[name] = 1
-
-    def __getattribute__(self, name):
-        if name[0] != '_':
-            return Omnibus.attrs.get(name, 0)
+    exempl = {}
+    def __setattr__(self, attr, n):
+        if attr in Omnibus.exempl:
+            Omnibus.exempl[attr].add(self)
         else:
-            return super().__getattribute__(name)
+            Omnibus.exempl[attr] = set([self])
 
-    def __delattr__(self, name):
-        if name in self.__dict__:
-            super().__delattr__(name)
-            Omnibus.attrs[name] -= 1
+    def __getattribute__(self, attr):
+        if attr in Omnibus.exempl:
+            return len(Omnibus.exempl[attr])
+        return 0
+
+    def __delattr__(self, attr):
+        if attr in Omnibus.exempl:
+            Omnibus.exempl[attr].discard(self)
 
 import sys
 exec(sys.stdin.read())
