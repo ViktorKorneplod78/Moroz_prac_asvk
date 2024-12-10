9a0af71a6502765fa0da935cc8b5b75f29bf4a6f
Viktor Korneplod <m.v.korneplod@gmail.com>
2024-12-10 11:55:48 +0300

    hawk tuah

--- 20241210/0/queue.py
+++ 20241210/0/queue.py
@@ -2,23 +2,21 @@ import asyncio
 
 async def prod(q1):
     for i in range(5):
+        await q1.put(f'value {i}')
+        print(f'prod: put {i} to q1')
         await asyncio.sleep(1)
-        await q1.put(f"value_{i}")
-        print(f"prod: put value_{i} to q1")
-        
-
 
 async def mid(q1, q2):
     while True:
-        val = await q1.get()
-        print(f"mid: got {val} from q1")
-        await q2.put(val)
-        print(f"mid: put {val} to q2")
+        a = await q1.get()
+        print(f'mid: got {a} from q1')
+        await q2.put(a)
+        print(f'mid: put {a} to q2')
 
 async def cons(q2):
     while True:
-        val = await q2.get()
-        print(f"cons: got {val} from q2")
+        a = await q2.get()
+        print(f'cons: got {a} from q2')
 
 async def main():
     q1 = asyncio.Queue()
@@ -27,5 +25,7 @@ async def main():
     mtask = asyncio.create_task(mid(q1, q2))
     ctask = asyncio.create_task(cons(q2))
     await ptask
+    mtask.cancel()
+    ctask.cancel()
 
 asyncio.run(main())
