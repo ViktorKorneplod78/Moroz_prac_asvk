7efdd1b8f4eae0b19182200ce9cc6c1b256fc0e8
Viktor Korneplod <m.v.korneplod@gmail.com>
2025-02-17 15:15:00 +0300

    hawk tuah 2

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -3,4 +3,11 @@ from glob import iglob
 from os.path import dirname, basename
 import sys
 
-print('\n'.join([basename(i) for i in iglob(sys.argv[1] + "/.git/refs/heads/*")]))
+path = sys.argv[1]
+
+if len(sys.argv) == 2:
+    print('\n'.join([basename(i) for i in iglob(path + "/.git/refs/heads/*")]))
+else:
+    comm_id = open(path + '/.git/refs/heads/' + sys.argv[2]).read()[:-1]
+    obj = decompress(open(path + f'/.git/objects/{comm_id[:2]}/{comm_id[2:]}', 'rb').read())
+    print(obj.partition(b'\x00')[2].decode(), end = "")
