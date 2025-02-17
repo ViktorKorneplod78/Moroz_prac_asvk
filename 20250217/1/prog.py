a36a4ba43df87467b9d2b13f2305edf341996bcd
Viktor Korneplod <m.v.korneplod@gmail.com>
2025-02-17 15:35:00 +0300

    hawk tuah 3

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -3,11 +3,34 @@ from glob import iglob
 from os.path import dirname, basename
 import sys
 
+def get_obj(path, obj_id):
+    return decompress(open(path + f'/.git/objects/{obj_id[:2]}/{obj_id[2:]}', 'rb').read()).partition(b'\x00')
+
+def parse_tree(path, body):
+    tree = []
+    tail = body
+    while tail:
+        treeobj, _, tail = tail.partition(b'\x00')
+        tmode, tname = treeobj.split()
+        num, tail = tail[:20], tail[20:]
+        obj_id = num.hex()
+        obj_name = get_obj(path, obj_id)[0].decode().split()[0]
+        tree.append([obj_name, obj_id, tname.decode()])
+    return tree
+
 path = sys.argv[1]
 
 if len(sys.argv) == 2:
+    # 1st
     print('\n'.join([basename(i) for i in iglob(path + "/.git/refs/heads/*")]))
 else:
-    comm_id = open(path + '/.git/refs/heads/' + sys.argv[2]).read()[:-1]
-    obj = decompress(open(path + f'/.git/objects/{comm_id[:2]}/{comm_id[2:]}', 'rb').read())
-    print(obj.partition(b'\x00')[2].decode(), end = "")
+    branch = sys.argv[2]
+    comm_id = open(path + '/.git/refs/heads/' + branch).read()[:-1]
+    comm_body = get_obj(path, comm_id)[2]
+    # 2nd
+    print(comm_body.decode(), end = "")
+
+    # 3d
+    tree_id = comm_body.decode().split('\n')[0].split()[1]
+    header, _, body = get_obj(path, tree_id)
+    print('\n'.join([' '.join(i) for i in parse_tree(path, body)]), end = ' ')
